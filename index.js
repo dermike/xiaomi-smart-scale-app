4d75a4a2d4368cc041b84f43b7961444f810311b
Mikael Jergefelt <mikael.jergefelt@ki.se>
2016-03-02 20:16:52 +0100

    only show values once

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const noble = require('noble');
+let lastvalue = '';
 
 noble.on('stateChange', state => {
   if (state === 'poweredOn') {
@@ -12,6 +13,7 @@ noble.on('stateChange', state => {
 noble.on('discover', peripheral => {
   let serviceData = peripheral.advertisement.serviceData,
     unit = '',
+    newval = false,
     measured;
   if (serviceData && serviceData.length) {
     for (let i in serviceData) {
@@ -20,18 +22,21 @@ noble.on('discover', peripheral => {
         switch (data.substr(0, 2)) {
         case '03':
           unit = 'lbs';
+          newval = true;
           break;
         case 'a3':
           unit = 'lbs';
           break;
         case '12':
           unit = 'jin';
+          newval = true;
           break;
         case 'b2':
           unit = 'jin';
           break;
         case '22':
           unit = 'kg';
+          newval = true;
           break;
         case 'a2':
           unit = 'kg';
@@ -43,7 +48,12 @@ noble.on('discover', peripheral => {
         measured = parseInt(data.slice(4, 6) + data.slice(2, 4), 16) * 0.01;
         measured = unit === 'kg' ? measured / 2 : measured;
         if (unit) {
-          console.log(measured + ' ' + unit);
+          if (lastvalue !== measured && !newval) {
+            console.log('Last measured: ' + measured + ' ' + unit);
+          } else if (newval && lastvalue !== measured) {
+            console.log('New measure: ' + measured + ' ' + unit);
+          }
+          lastvalue = measured;
         }
       }
     }
